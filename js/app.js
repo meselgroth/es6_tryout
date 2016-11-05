8b409dff2ca80d0b8a242536395ed27dc8f15f83
meselgroth <meselgroth@yahoo.com>
2016-11-05 13:11:03 +0800

    New class syntax. Only constructors, get/set and methods allowed inside. No encapsulation of this.variables

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
     console.log(hello);
     console.log(goodbye);
     
+    
     function LiteralShortHand(){
         let hello = "hello";
         let goodbye = "goodbye";
@@ -14,4 +15,30 @@
         return { hello, goodbye };
     }
 
+    function callFunction(callback){
+        callback();
+    };
+    callFunction(() => { console.log(hello + ' again')});
+    
+    class Phrase{
+        
+        constructor(langA, langB){
+            this._langA = langA;
+            this._langB = langB;
+        }
+        get LangA(){
+            return this._langA;
+        }
+        printMe(){
+            console.log("printMe");
+            console.log("langA" + this._langA);
+            console.log("langB" + this._langB);
+        }
+    }
+    
+    let phrase = new Phrase("good day", "Guten Tag");
+    console.log("1 langA" + phrase._langA);
+    console.log("2 langA" + phrase.LangA);
+    phrase._langB = "ciao"
+    phrase.printMe();
 })();
